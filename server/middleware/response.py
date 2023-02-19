501afd9ac417c730d9df8edff86b86156f94f96d
julian-salinas <juliansalinasm@gmail.com>
2023-02-19 00:16:47 -0300

    Refactor de middleware

--- server/middleware/response.py
+++ server/middleware/response.py
@@ -3,24 +3,32 @@
 from fastapi.responses import JSONResponse
 from .handlers.handlers import map_handler
 
-def __obj_to_dict__(obj):
+def _obj_to_dict(obj):
     """ Convierte un objeto en un diccionario. """
     if isinstance(obj, (int, float, str, bool)):
         return obj
     elif isinstance(obj, dict):
-        return {k: __obj_to_dict__(v) for k, v in obj.items()}
+        return {k: _obj_to_dict(v) for k, v in obj.items()}
     elif isinstance(obj, (list, tuple, set)):
-        return [__obj_to_dict__(e) for e in obj]
+        return [_obj_to_dict(e) for e in obj]
     else:
-        return {k: __obj_to_dict__(v) for k, v in vars(obj).items()}
+        return {k: _obj_to_dict(v) for k, v in vars(obj).items()}
 
 
-async def response_handler(request, call_next):
+async def _handle_exception(request, e):
+    return map_handler(e)(request, e)
+
+
+async def _handle_json_response(response):
     try:
-        response = await call_next(request)
-    except Exception as e:
-        return map_handler(e)(request, e)
+        return JSONResponse(
+            content=_obj_to_dict(response), 
+            status_code=response.status_code if hasattr(response, "status_code") else 200)
+    except:
+        return response
 
+
+async def _handle_plain_response(request, response):
     if isinstance(response, JSONResponse):
         return response
 
@@ -33,20 +41,24 @@ async def response_handler(request, call_next):
     if response.status_code == 500:
         if request.app.debug:
             return JSONResponse(
-                content = __obj_to_dict__(response),
-                status_code = response.status_code if hasattr(response, "status_code") else 500)
+                content=_obj_to_dict(response),
+                status_code=response.status_code if hasattr(response, "status_code") else 500)
         else:
             return JSONResponse(
                 {"message": "An internal server error has occurred"},
                 status_code=500)  
 
+    return response
+
+
+async def response_handler(request, call_next):
+    try:
+        response = await call_next(request)
+    except Exception as e:
+        return await _handle_exception(request, e)
+
     content_type = response.headers.get("content-type")
     if content_type and "application/json" in content_type:
-        try:
-            return JSONResponse(
-                content=__obj_to_dict__(response), 
-                status_code=response.status_code if hasattr(response, "status_code") else 200)
-        except:
-            return response
-
-    return response
+        return await _handle_json_response(response)
+    else:
+        return await _handle_plain_response(request, response)
