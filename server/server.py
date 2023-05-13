62e88c2e8dcc1bec5f069255b0bd4b0256cbc6b3
julian-salinas <juliansalinasm@gmail.com>
2023-05-13 17:35:18 -0300

    Agrego redirecciones

--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ from .config import *
 from .middleware import response_handler
 
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from mongoengine import connect, errors
 
@@ -25,6 +26,12 @@ app.add_middleware(
 )
 
 app.middleware("http")(response_handler)
+
+# redirect / and /docs to /index.html
+app.add_api_route("/", endpoint=lambda: RedirectResponse(url="/index.html"), include_in_schema=False)
+app.add_api_route("/docs", endpoint=lambda: RedirectResponse(url="/index.html"), include_in_schema=False)
+
+# /api routes
 app.include_router(api_router, prefix = "/api")
 
 if mode == "development":
